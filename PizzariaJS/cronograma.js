06aa878e98a500530e25732effbdeb004e0ac44d
EduardoMoraesRitter <eduardo.moraes.ritter@gmail.com>
2016-05-09 17:49:34 -0300

    novos

--- PizzariaJS/cronograma.js
+++ PizzariaJS/cronograma.js
@@ -2,7 +2,45 @@
 novo site usando JS (em node ou em js puro)
 pizzaria.JS
 
-desiner patter - Strategy (Reduzindo de ifs)
+
+
+*********Algoritimos********
+
+exemplo:
+de PID - o robo bob
+
+algoritimo de formiga
+algoritimo da abelha
+
+problema das 8 rainhas
+
+problema do cacheiro viajante
+
+
+
+*********desiner patter********
+
+- Strategy (Reduzindo de ifs)
 exemplo:
 http://blog.alura.com.br/reduzindo-de-n-ifs-para-nenhum-com-strategy-em-java/
+
+
+
+*********comando********
+
+diferença entra dos dois for
+
+um sendo recursivo e o outro mais rapido por nao precisar contar
+
+for (var j = 0; j < GearSettingsVariation.VariationSelected.length; j++) {
+     
+     GearSettingsVariation.VariationSelected.splice(j, 1);
+
+for (var i = 0, innerJoinlength = innerJoin.length; i < innerJoinlength; i++) {
+    
+    
+    
+    
+    
+
 */
