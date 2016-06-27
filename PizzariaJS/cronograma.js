e917bfc36106244e488d30d02fa22b26140399a0
EduardoMoraesRitter <eduardo.moraes.ritter@gmail.com>
2016-06-26 23:17:03 -0300

    cronograma

--- PizzariaJS/cronograma.js
+++ PizzariaJS/cronograma.js
@@ -1,7 +1,36 @@
-/*
+﻿/*
 novo site usando JS (em node ou em js puro)
 pizzaria.JS
 
+aulas com nas pratica com exercicios do URI(https://www.urionlinejudge.com.br/judge/en/problems/index/1)
+Nivel Beginner
+
+-- html basico
+-- acesso ao html com js
+-- CSS3
+-- HTML 5
+-- if's
+-- loop
+--- array
+
+--Orientação objeto
+--SOLID
+--Desiner Pather 23 pontos
+
+--Banco de Dodos local
+---localstoreg
+
+--Angular
+--Ionic
+--NOSQL
+--Phonegap
+
+
+*********desiner patter********
+
+- Strategy (Reduzindo de ifs)
+exemplo:
+http://blog.alura.com.br/reduzindo-de-n-ifs-para-nenhum-com-strategy-em-java/
 
 
 *********Algoritimos********
@@ -15,15 +44,6 @@ algoritimo da abelha
 problema das 8 rainhas
 
 problema do cacheiro viajante
-
-
-
-*********desiner patter********
-
-- Strategy (Reduzindo de ifs)
-exemplo:
-http://blog.alura.com.br/reduzindo-de-n-ifs-para-nenhum-com-strategy-em-java/
-
 
 
 *********comando********
